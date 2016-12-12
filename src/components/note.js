4c0b10226078d41b5a4127f003ac453c8d16fc2b
Malinda Wellalage <malinda.iw@gmail.com>
2016-12-12 23:56:55 +0530

    Initialize project

---
+++ src/components/note.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../css/App.css';
+
+class Note extends Component {
+  render() {
+    return (
+      //TODO
+    );
+  }
+}
+
+export default Note;
